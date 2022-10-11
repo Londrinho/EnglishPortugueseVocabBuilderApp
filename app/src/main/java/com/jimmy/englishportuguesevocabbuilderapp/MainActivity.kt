6aadc3c5a03335815d0d8f7f794da4f979775ce0
JimmyApara <jimmyapara@gmail.com>
2022-10-11 15:52:11 +0100

    Implemented an overflow menu. Got to work on the look of the layouts in the recyclerview but it is compiling.

--- app/src/main/java/com/jimmy/englishportuguesevocabbuilderapp/MainActivity.kt
+++ app/src/main/java/com/jimmy/englishportuguesevocabbuilderapp/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.jimmy.englishportuguesevocabbuilderapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import com.jimmy.englishportuguesevocabbuilderapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+//        val navController = this.findNavController(R.id.myHostFragment)
+//        NavigationUI.setupActionBarWithNavController(this,navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.overflow_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.myHostFragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = this.findNavController(R.id.myHostFragment)
+//        return navController.navigateUp()
+//    }
 }
